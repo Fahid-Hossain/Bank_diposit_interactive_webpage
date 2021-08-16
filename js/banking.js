30018d36d3e05bae13b00df5901f203b3f611907
Fahid Hossain <fahidhossain2021@gmail.com>
2021-08-16 20:46:49 +0600

    diposit & withdraw display update function and balance update function

--- js/banking.js
+++ js/banking.js
@@ -7,14 +7,30 @@ function getInputId(inputId){
   return inputValueConverted;
 }
 
-// get display diposit and withdraw function 
-/* function getDisplayId(displayId){
+// get update and display diposit and withdraw function
+
+ function getDisplayId(displayId,inputAmount){
   const displayAmount = document.getElementById(displayId);
   const displayAmountText = displayAmount.innerText;
   const displayAmountConverted = parseFloat(displayAmountText);
-  return displayAmountConverted;
-}
- */
+  displayAmount.innerText = displayAmountConverted + inputAmount;
+ }
+ 
+ // get balance update for deposit and withdraw 
+ function balanceUpdate(AmountConverted,isAdd){
+  const balanceAmountDisplay = document.getElementById("balance_amount_display");
+  const balanceAmountText = balanceAmountDisplay.innerText;
+  const balanceAmountConverted = parseFloat(balanceAmountText);
+
+  if(isAdd == true){
+    const totalBalance = balanceAmountConverted + AmountConverted ;
+    balanceAmountDisplay.innerText = totalBalance;
+  }
+  else{
+    const totalBalance = balanceAmountConverted - AmountConverted ;
+    balanceAmountDisplay.innerText = totalBalance;
+  }
+ }
 // ----Diposit >> get diposit ----------//-- 
 document.getElementById("diposit_btn").addEventListener("click",function(){
     // get diposit input 
@@ -26,7 +42,7 @@ document.getElementById("diposit_btn").addEventListener("click",function(){
     dipositInput.value = ""; */
     const dipositAmountConverted = getInputId("diposit_input");
     // diposit amount display 
-    const dipositAmountDisplay = document.getElementById("diposit_amount_display");
+/*     const dipositAmountDisplay = document.getElementById("diposit_amount_display");
     const dipositAmountText = dipositAmountDisplay.innerText;
     const dipositAmountDisplayConverted = parseFloat(dipositAmountText);
     // console.log(dipositAmountDisplayConverted);
@@ -35,20 +51,24 @@ document.getElementById("diposit_btn").addEventListener("click",function(){
     const dipositTotal = dipositAmountDisplayConverted + dipositAmountConverted ;
     
        // add to display diposit amount 
-       dipositAmountDisplay.innerText = dipositTotal;
+       dipositAmountDisplay.innerText = dipositTotal; */
+
+       const totalDiposit = getDisplayId("diposit_amount_display",dipositAmountConverted);
 
 
      ///---------///---------Balance update for diposit---------///--------///
     // get balance amount
-     const balanceAmountDisplay = document.getElementById("balance_amount_display");
+ /*      const balanceAmountDisplay = document.getElementById("balance_amount_display");
      const balanceAmountText = balanceAmountDisplay.innerText;
      const balanceAmountConverted = parseFloat(balanceAmountText);
     //  console.log(balanceAmountConverted);
 
     const totalBalance = balanceAmountConverted + dipositAmountConverted ;
-    balanceAmountDisplay.innerText = totalBalance;
+    balanceAmountDisplay.innerText = totalBalance; */
 
-})
+   balanceUpdate(dipositAmountConverted,true);
+
+ })
 
 // --- withdraw >> get withdraw ----//----
 
@@ -65,25 +85,29 @@ document.getElementById("withdraw_btn").addEventListener("click",function(){
     const withdrawAmountConverted = getInputId("withdraw_input");
 
     // get withdraw Amount
-    const withdrawAmountDisplay = document.getElementById("withdraw_amount_display");
+  /*   const withdrawAmountDisplay = document.getElementById("withdraw_amount_display");
     const withdrawAmountDisplayText = withdrawAmountDisplay.innerText;
     const withdrawAmountDisplayConverted = parseFloat(withdrawAmountDisplayText);
     // console.log(withdrawAmountDisplayConverted);
 
     // (withdraw input amount + withdraw previous amount)
-    const totalWithdraw = withdrawAmountDisplayConverted + withdrawAmountConverted;
+    const totalWithdraw = withdrawAmountDisplayConverted + withdrawAmountConverted; */
+
+    const totalWithdraw = getDisplayId("withdraw_amount_display",withdrawAmountConverted);
 
     // withdraw Amount Display 
-    withdrawAmountDisplay.innerText = totalWithdraw;
+    // withdrawAmountDisplay.innerText = totalWithdraw;
 
 
      ///---------///---------Balance update for withdraw---------///--------///
      
   // get balance amount
-  const balanceAmountDisplay = document.getElementById("balance_amount_display");
+/*   const balanceAmountDisplay = document.getElementById("balance_amount_display");
   const balanceAmountText = balanceAmountDisplay.innerText;
   const balanceAmountConverted = parseFloat(balanceAmountText);
 
   const totalBalance = balanceAmountConverted - withdrawAmountConverted;
-    balanceAmountDisplay.innerText = totalBalance;
+    balanceAmountDisplay.innerText = totalBalance; */
+
+    balanceUpdate(withdrawAmountConverted,false);
 })
